c76bfe5b0263898cf6b22b319b7ceb337af721d5
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-07-29 04:19:41 -0500

    Created database.js, which starts the connection to the database and sets up the models

---
+++ db/database.js
@@ -0,0 +1,46 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const SetupShoppingCartModel = require('./models/shopping-cart')
+const SetupCartProductModel = require('./models/cart-product')
+
+const config = {
+    database: process.env.DB_NAME || '',
+    username: process.env.DB_USER || '',
+    password: process.env.DB_PASS || '',
+    host: process.env.DB_HOST || '',
+    dialect: 'postgres',
+    setup: true,
+    logging: console.log,
+    define: {
+        underscored: true,
+        freezeTableName: true,
+        paranoid: true
+    }
+}
+
+module.exports = async function () {
+
+    const sequelize = new Sequelize(config)
+
+    await sequelize.authenticate()
+        .then(() => {
+            console.log('Connection has been established successfully.');
+        })
+        .catch(err => {
+            console.error('Unable to connect to the database:', err);
+        });
+
+    const ShoppingCart = SetupShoppingCartModel(sequelize)
+    const CartProduct = SetupCartProductModel(sequelize)
+
+    ShoppingCart.hasMany(CartProduct, { onDelete: 'CASCADE'})
+    CartProduct.belongsTo(ShoppingCart)
+
+    if (config.setup) sequelize.sync({ force: true, logging: console.log })
+
+    return {
+        ShoppingCart,
+        CartProduct
+    }
+}
