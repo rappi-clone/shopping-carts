075b756f62d2042e3f2cef3d35d122570d5fb710
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-07-29 04:22:49 -0500

    Fixed syntax error

--- db/lib/errorHandler.js
+++ db/lib/errorHandler.js
@@ -45,6 +45,7 @@ module.exports = (error, next) => {
         // if (error.message.includes('invalid input syntax for type numeric:')) next({ error: new Error('Debes ingresar un precio válido'), status: 401 })
         // if (error.message.includes('invalid input syntax for type time:')) next({ error: new Error('Debes ingresar una hora válida'), status: 401 })
         // if (error.message.includes('invalid input syntax for integer:')) next({ error: new Error('Debes ingresar una cantidad disponible válida'), status: 401 })
-        else next({ error: new Error(error.message), status: 500 })
+        // else 
+        next({ error: new Error(error.message), status: 500 })
     }
 }
