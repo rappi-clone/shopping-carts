500de6ef65d1811f3726a6e549b4e7aeb04c55ac
Sergio Álvarez Gómez <sf.alvarez10@uniandes.edu.co>
2019-07-29 04:24:44 -0500

    Created server.js which starts the express server, redirects http requests to the routes file and handles errors that show up during the handling of the requests

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict'
+
+const Express = require('express');
+const app = Express();
+const PORT = 4001;
+const database = require('./db/database')();
+const routes = require('./routes/routes')(database)
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Access-Control-Request-Headers, Access-Control-Request-Method, Origin, X-Requested-With, Content-Type, Accept, DNT, Referer, User-Agent, Authorization");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    }
+    else {
+        next();
+    }
+});
+
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+
+app.use('/', routes);
+
+//Handle errors
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    if (err.error) res.json({ errorMessage: err.error.message });
+    else {
+        res.json({ errorMessage: 'Ocurrió un error en el servidor. Inténtalo de nuevo en un momento' });
+        if (err.errors && err.errors[0]) console.log(err.errors[0].message)
+        // else console.log(err)
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at port: ${PORT}`)
+});
